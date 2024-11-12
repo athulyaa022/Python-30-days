28d3883348eb030acfce2e00814a9aa2f337c436
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-11-12 22:09:39 +0530

    Add files via upload

---
+++ Day-1/Alexday1.py
@@ -0,0 +1,15 @@
+'''
+Author:Alex
+Date:12-11-2024
+Description:The print() function prints the specified message to the screen or other standard output devices.
+ The message can be a string or any other object; the object will be converted into a string before being
+ displayed.
+'''
+str_1="My name is Alex"
+str_2="I am 20 years old "
+str_3="I like python"
+print(str_1)
+print(str_2)
+print(str_3)
+print(str_1,"and ",str_2)
+print(str_1[11:15],"",str_1[8:10],"",str_2[4:17])
