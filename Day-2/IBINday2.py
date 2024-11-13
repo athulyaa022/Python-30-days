d29bd70f50e398b50e6e30c82f3931161f3f4230
Ibin Siby <ibinsiby4619@gmail.com>
2024-11-13 22:15:15 +0530

    Add files via upload

---
+++ Day-2/IBINday2.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+
+result=(num1+num2)
+print("The sum 0f num1 and num2 is:",result)
+
+print("The difference when num1 is subtracted from num2 is:",num1-num2)
+print("The product of num1 and num2 is:",num1*num2)
+print("The quotient when num1 is divided by num2 is:",num1/num2)
+
