c9cd045bbc452909a762081b462e67ee979bfd90
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-11-13 21:51:50 +0530

    Add files via upload

---
+++ Day-2/Alexday2.py
@@ -0,0 +1,13 @@
+'''
+Author:Alex
+Date:13-11-2024
+Description:The input() function allows you to take input from the user.
+ It always returns the input as a string, so you’ll need to convert it to the appropriate data type
+ (like int or float)if you want to perform arithmetic with it.
+'''
+number1=int(input("Enter the first number:"))
+number2=int(input("Enter the second number:"))
+print("Sum:",number1+number2)
+print("Difference:",number1-number2)
+print("Product:",number1*number2)
+print("Quotient:",number1/number2)
