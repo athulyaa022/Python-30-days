8faedcabca45fc03d171c21d4f655fd997925f34
diyakallarackal <diyakallarackal@gmail.com>
2024-11-14 00:00:20 +0530

    Create diyaday2.py

---
+++ Day-2/diyaday2.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+sum=num1+num2
+difference=num1-num2
+product=num1*num2
+quotient=num1/num2
+print("Sum:",sum)
+print("Difference:",difference)
+print("Product:",product)
+print("Quotient:",quotient)
