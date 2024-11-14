ae2aeeb2c4793bda982289a29f530a2e46941942
Ibin Siby <ibinsiby4619@gmail.com>
2024-11-14 21:47:22 +0530

    Update IBINday3.py

--- Day-3/IBINday3.py
+++ Day-3/IBINday3.py
@@ -1,9 +1,9 @@
 age=int(input("Enter your age:"))
 if(age<13):
     print("You are a Child")
-elif(age>13 and age<19):
+elif(age>=13 and age<=19):
     print("You are a Teen")
-elif(age>20 and age<64):
+elif(age>=20 and age<=64):
     print("You are an Adult")
 else:
     print("You are a Senior")
