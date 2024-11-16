625c27f09e6d3e9e02a57a29df955a3cde4f959f
athulyaa022 <athulyaa2028@cs.sjcetpalai.ac.in>
2024-11-16 19:33:20 +0530

    Add files via upload

---
+++ Day-4/athulyaday4task2.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age (in years): "))
+age_in_months = age * 12
+print("Hello," +name+"!")
+print("You are",age,"years old, which is",age_in_months,"months.")
