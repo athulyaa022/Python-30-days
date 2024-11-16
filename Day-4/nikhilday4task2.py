1e84c9e29ce5f735b58f1250257d978e3776b593
nikhilsjcet <nikhilsaju2028@cs.sjcetpalai.ac.in>
2024-11-16 12:11:43 +0530

    Add files via upload

---
+++ Day-4/nikhilday4task2.py
@@ -0,0 +1,4 @@
+name=input("Enter your name:")
+age=int(input("Enter your age(in years):"))
+age_in_months=(age*12)
+print("Hello,"+name+"!""\nYou are",age,"years old, which is",age_in_months,"months.")
