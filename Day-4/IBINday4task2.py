543dc05b22c1b828312e5f027e8925c9b2e0eb35
Ibin Siby <ibinsiby4619@gmail.com>
2024-11-16 14:41:36 +0530

    Add files via upload

---
+++ Day-4/IBINday4task2.py
@@ -0,0 +1,7 @@
+name=str(input("Enter your name:"))
+age=int(input("Enter your age(in years):"))
+age_in_months=(age*12)
+print("Hello,",name)
+print("You are",age,"years old,which is",age_in_months,"months.")
+
+
