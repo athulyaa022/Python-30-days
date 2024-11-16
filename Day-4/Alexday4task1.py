b16b939218d5a9f6e84428735c0956e449c39601
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-11-16 18:20:31 +0530

    Add files via upload

---
+++ Day-4/Alexday4task1.py
@@ -0,0 +1,9 @@
+'''
+Author:Alex
+Date:16-11-2024
+Description:The input() function allows users to enter information, which can then be processed or displayed.
+By combining input() with print(), you can create interactive programs that respond to user-provided data.
+'''
+name=input("Enter your  name:")
+age=int(input("Enter your age:"))
+print("Hell0,"+name+"! You are",age,"years old.")
