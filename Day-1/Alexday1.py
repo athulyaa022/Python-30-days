cec80ee1127751218924f910f522fd5e55aa3a9b
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-11-16 18:21:42 +0530

    Update Alexday1.py

--- Day-1/Alexday1.py
+++ Day-1/Alexday1.py
@@ -5,11 +5,5 @@ Description:The print() function prints the specified message to the screen or o
  The message can be a string or any other object; the object will be converted into a string before being
  displayed.
 '''
-str_1="My name is Alex"
-str_2="I am 20 years old "
-str_3="I like python"
-print(str_1)
-print(str_2)
-print(str_3)
-print(str_1,"and ",str_2)
-print(str_1[11:15],"",str_1[8:10],"",str_2[4:17])
+print("I Love Python")
+
