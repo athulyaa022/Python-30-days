181d6fe52229ff115d9754c4ec7362342e0b000a
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-11-16 18:40:40 +0530

    Add files via upload

---
+++ Day-4/Alexday4task2.py
@@ -0,0 +1,11 @@
+'''
+Author:Alex
+Date:16-11-2024
+Description:In this task, you'll practice using the input() function and
+ also work with formatted strings to display a more structured output.
+ You'll calculate the user's age in months and display it alongside their name.
+'''
+name=input("Enter your name: ")
+age=int(input("Enter your age (in years): "))
+print("Hello,"+name+"!")
+print("You are",age,"years old, which is",age*12,"months.")
