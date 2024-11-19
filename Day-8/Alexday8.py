e87e681bbfd04a3ea9bcb8393e762e3418e8e273
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-11-20 04:47:34 +0530

    Update Alexday8.py

--- Day-8/Alexday8.py
+++ Day-8/Alexday8.py
@@ -3,11 +3,11 @@ Author:Alex
 Date:20-11-2024
 Description: Write a program to identify and print all even and odd numbers within a specified range.
 '''
-n="Enter the start of the range"
-n="Enter the end of the range"
-print(n)
-print(n)
-for n in range(1,6):
+num_1=int(input("Enter the start of the range"))
+num_2=int(input("Enter the end of the range"))
+print(num_1)
+print(num_2)
+for n in range(num_1,num_2+1):
     if n%2==0 :
         print(n,"is even")
     elif n%2!=0 :
