bf464f129907b888ffbcf00ce1c5ae51e193794a
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-11-20 04:24:40 +0530

    Add files via upload

---
+++ Day-7/Alexday7.py
@@ -0,0 +1,15 @@
+'''
+Author:Alex
+Date:19-11-2024
+Description:Write a program that counts from 1 to 10 and uses break, continue,
+ and pass in specific situations.
+'''
+for n in range(1,11):
+    if n == 4:
+        continue
+    elif n ==4:
+        pass
+    elif n ==9:
+        break
+    else:
+       print(n)
