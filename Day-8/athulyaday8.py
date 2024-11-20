4c8f55f59e1c755a431c371fb43973f56462bc97
athulyaa022 <athulyaa2028@cs.sjcetpalai.ac.in>
2024-11-20 20:40:46 +0530

    Add files via upload

---
+++ Day-8/athulyaday8.py
@@ -0,0 +1,7 @@
+start = int(input("Enter the start of the range: "))
+end = int(input("Enter the end of the range: "))
+for num in range(start, end + 1):
+    if num % 2 == 0:
+        print(f"{num} is Even")
+    else:
+        print(f"{num} is Odd")
