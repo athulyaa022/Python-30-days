cd0082b428a97d93dc364376be8e70d5b972f074
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-11-24 10:08:34 +0530

    Add files via upload

---
+++ Day-10/Alexday10.py
@@ -0,0 +1,15 @@
+'''
+Author:Alex
+Date:23-11-2024
+Description:: Write a program that calculates and prints the sum of
+all numbers from 1 to a user-specified number (N) using a while loop.
+'''
+N=int(input("Enter a number"))
+i=1
+total_sum=0
+while i<=N:
+    total_sum +=i
+    i +=1
+print("Total sum of numbers from 1 to 5 is:",total_sum)
+    
+
