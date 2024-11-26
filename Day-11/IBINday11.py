8eb86623be35bb3be364de399aee0544404d98dd
Ibin Siby <ibinsiby4619@gmail.com>
2024-11-26 21:44:23 +0530

    Add files via upload

---
+++ Day-11/IBINday11.py
@@ -0,0 +1,12 @@
+numbers=input("Enter numbers separated by spaces:")
+numbers=numbers.split()
+even_count=0
+odd_count=0
+for i in numbers:
+    i=int(i)
+    if i%2==0:
+        even_count=even_count+1
+    else:
+        odd_count=odd_count+1
+print("Even numbers:",even_count)
+print("Odd numbers:",odd_count)
