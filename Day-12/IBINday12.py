e8ab8a54e198b8d01596e91075f6900d4e4a8527
Ibin Siby <ibinsiby4619@gmail.com>
2024-11-26 21:45:19 +0530

    Add files via upload

---
+++ Day-12/IBINday12.py
@@ -0,0 +1,7 @@
+number=int(input("Enter a positive integer: "))
+sum_of_the_digits=0
+while number>0:
+    last_digit=number%10
+    sum_of_the_digits=sum_of_the_digits+last_digit
+    number//=10
+print("The sum of the digits is:",sum_of_the_digits)
