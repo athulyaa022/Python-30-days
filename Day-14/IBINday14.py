a9b4403fc87cbc92c92f10f2ef7e0d633228dd1d
Ibin Siby <ibinsiby4619@gmail.com>
2024-11-27 19:20:15 +0530

    Update IBINday14.py

--- Day-14/IBINday14.py
+++ Day-14/IBINday14.py
@@ -1,4 +1,4 @@
-number=int(input("Enter a number for the multiplication table:"))
+number=int(input("Enter a number to generate its multiplication table:"))
 for i in range(1,11):
     result=number*i
     print(number,"*",i,"=",result)
