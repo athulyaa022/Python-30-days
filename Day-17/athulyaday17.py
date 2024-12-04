da7428df8afead7309964839b8fe8dc5cc0c1506
athulyaa022 <athulyaa2028@cs.sjcetpalai.ac.in>
2024-12-04 22:05:45 +0530

    Add files via upload

---
+++ Day-17/athulyaday17.py
@@ -0,0 +1,11 @@
+my_tuple = (1, "Python", 3.14, "Code", 42)
+print("The tuple is:", my_tuple)
+first_three = my_tuple[:3]
+print("The first three elements are:", first_three)
+last_two = my_tuple[-2:]
+print("The last two elements are:", last_two)
+a, b, c = my_tuple[:3]
+print("Unpacked values:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
